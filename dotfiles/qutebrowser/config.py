81e1752879e8e3eb42874ecaad55f21408ceee92
James SETKEH Griffis <setkeh@gmail.com>
2021-10-08 20:41:56 +1100

    Adding custom Qutebrowser config

--- dotfiles/qutebrowser/config.py
+++ dotfiles/qutebrowser/config.py
@@ -1,2 +1,9 @@
+#Config.py Overrides Autoconfig.yml
+config.load_autoconfig()
+
 # Configure Window Layout
 c.tabs.position = "left"
+
+# Define Keybinds
+config.bind('C', 'spawn --userscript ~/.config/nixpkgs/dotfiles/qutebrowser/1pass.py fill_credentials --cache-session')
+config.bind('V', 'spawn --userscript ~/.config/nixpkgs/dotfiles/qutebrowser/1pass.py fill_totp --cache-session')
