ca9faa20f34637e30c3298a856d514da9e9c8756
James SETKEH Griffis <setkeh@gmail.com>
2022-03-04 11:28:15 +1100

    Pushing laptop hardware

--- hosts/laptop/laptop.nix
+++ hosts/laptop/laptop.nix
@@ -9,16 +9,21 @@
   boot.initrd.kernelModules = [ "dm-snapshot" ];
 
   fileSystems."/" = {
-      device = "/dev/disk/by-uuid/...";
-      fsType = "ext4";
-    };
+    device = "/dev/disk/by-uuid/2f26a805-c31a-4f84-9f81-47bbbb9635f6";
+    fsType = "ext4";
+  };
 
   fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/...";
-      fsType = "vfat";
-    };
+    device = "/dev/disk/by-uuid/5EBA-18DF";
+    fsType = "vfat";
+  };
 
-  swapDevices =[{
-    device = "/dev/disk/by-uuid/...";
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/5ffcc34d-8019-4105-8245-9819751d8866";
+    fsType = "ext4";
+  };
+
+  swapDevices = [{
+    device = "/dev/disk/by-uuid/f4348d86-d71d-42be-ab12-41cf52eda734";
   }];
 }
