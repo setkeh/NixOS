f1ea17d2186a3d19c89f9dea206e6438fab290a7
James SETKEH Griffis <setkeh@gmail.com>
2026-03-30 13:23:48 +1100

    updating DWM overlay and switching WM

--- etc/overlays/dwm.nix
+++ etc/overlays/dwm.nix
@@ -1,15 +1,9 @@
-{config, pkgs, ...}:
-
-{
-  nixpkgs.overlays = [
-    (self: super: {
-      dwm = super.dwm.overrideAttrs (oldAttrs: rec {
-#        patches = [
-#        ./path/to/my-dwm-patch.patch
-#        ];
-      configFile = super.writeText "config.h" (builtins.readFile ../../dotfiles/dwm/config.h);
-      postPatch = oldAttrs.postPatch or "" + "\necho 'Using own config file...'\n cp ${configFile} config.def.h";
-      });
-    })
-  ];
+final: prev: {
+    dwm = prev.dwm.overrideAttrs (oldAttrs: rec {
+#      patches = [
+#      ./path/to/my-dwm-patch.patch
+#      ];
+    configFile = prev.writeText "config.h" (builtins.readFile ../../dotfiles/dwm/config.h);
+    postPatch = oldAttrs.postPatch or "" + "\necho 'Using own config file...'\n cp ${configFile} config.def.h";
+    });
 }
