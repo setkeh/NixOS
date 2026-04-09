ae095111420950779a0dd41ccdfc6a75d8350ebf
James SETKEH Griffis <setkeh@gmail.com>
2026-04-10 05:25:18 +1000

    attempting to switch to Sops agenix is causing drama

--- flake.nix
+++ flake.nix
@@ -2,31 +2,16 @@
   description = "My Home Manager configuration flake";
 
   inputs = {
-    #agenix = {
-    #  url = "github:ryantm/agenix";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # Use the appropriate branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-
-    /* overlays.default = final: prev: {
-        dwm = prev.callPackage ./etc/overlays/dwm.nix { };
-        slstatus = prev.callPackage ./etc/overlays/alstatus.nix { };
-      };
-
-      packages.x86_64-linux = {
-        inherit (pkgs) dwm;
-        inherit (pkgs) slstatus; 
-      }; */
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-wsl, sops-nix /*agenix*/, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nixos-wsl, sops-nix, ... }@inputs: {
     nixosConfigurations = {
       # WSL Configurationc
       nixos-e7250 = nixpkgs.lib.nixosSystem {
@@ -37,37 +22,21 @@
           /* Overlay Module */
           ./etc/overlays
 
-          /*agenix.nixosModules.default
-          ({ pkgs, ... }: {
-            environment.systemPackages = [ agenix.packages.x86_64-linux.default pkgs.age-plugin-yubikey pkgs.age ];
-          })*/
-
           sops-nix.nixosModules.sops
 
           ({ config, ...}: {
             sops = {
-              #age.keyFile = "${config.home.homeDirectory}/.identitys/age-yubikey-identity-44672097.txt"; # Relative to flake.nix
               age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-              defaultSopsFile = ./secrets/fish_alias.yaml;
+              defaultSopsFile = ./secrets/nixos-e7250.yaml;
               secrets = {
-                fish_aliases = {
+                /* Im not sure if there is a better way to do this */
+                "nixos-e7250/fish_aliases" = {
                   owner = config.users.users.setkeh.name;
                   path = "/home/setkeh/.config/fish/conf.d/alias.fish";
                 };
               };
             };
           })
-
-          /*({ lib, pkgs, ...}: {
-            age = {
-              ageBin = "PATH=$PATH:${lib.makeBinPath [ pkgs.age-plugin-yubikey ]} ${pkgs.age}/bin/age";
-              identityPaths = [ "/home/setkeh/.identitys/age-yubikey-identity-44672097.txt" ];
-              secrets = {
-                test-alias.file = ./secrets/fish-alias.age;
-                fish-alias.file = ./secrets/fish-alias.age;
-              };
-            };
-          })*/
         
           home-manager.nixosModules.home-manager
           ({ config, lib, pkgs, ...}: {
@@ -79,46 +48,11 @@
             home-manager.users.setkeh = { config, pkgs, ... }: {
               imports = [
                 ./home/nixos-e7250
-                #agenix.homeManagerModules.default
               ];
-
-              /*sops = {
-                age.keyFile = "${config.home.homeDirectory}/.identitys/age-yubikey-identity-44672097.txt"; # Relative to flake.nix
-                age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-                defaultSopsFile = ./secrets/fish_alias.yaml;
-                secrets = {
-                  "fish_aliases/nswitch" = {};
-                };
-              };*/
-
-              /*sops.secrets."fish-aliases" = {
-                path = "${config.home.homeDirectory}/.config/fish/sops_aliases.fish";
-              };*/
-
-              /*sops = {
-                age.keyFile = /home/setkeh/.identitys/age-yubikey-identity-44672097.txt;
-                defaultSopsFile = ./secrets/fish-alias.yaml;
-              };*/
-              /* age = {
-                #ageBin = "PATH=$PATH:${lib.makeBinPath [ pkgs.age-plugin-yubikey ]} ${pkgs.age}/bin/age";
-                identityPaths = [ "/home/setkeh/.identitys/age-yubikey-identity-44672097.txt" ];
-                secrets = {
-                  fish-alias.file = ./secrets/fish-alias.age;
-                };
-              };*/
             };
           })
         ];
       };
-    # Define a specific configuration for your user and system
-    #homeConfigurations."setkeh@nixos-e7250" = home-manager.lib.homeManagerConfiguration {
-    #  inherit pkgs;
-    #  /* modules = [ ./home.nix ]; # Imports your existing home.nix file */
-    #  # Add other necessary arguments like username, homeDirectory, etc.
-    #  home.username = "setkeh";
-    #  home.homeDirectory = "/home/setkeh";
-    #  home.stateVersion = "25.11"; # Match your state version
-    #};
+    };
   };
- };
 }
