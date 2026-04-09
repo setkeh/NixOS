b0c102b313c53ca75dadf1d32dc1d1a203e45d6e
James SETKEH Griffis <setkeh@gmail.com>
2026-04-10 00:56:15 +1000

    attempting to switch to Sops agenix is causing drama

--- common/applications/fish/alias.nix
+++ common/applications/fish/alias.nix
@@ -2,7 +2,7 @@
 
 {
   programs.fish.shellAliases = {
-    nswitch = builtins.readFile config.age.secrets.fish-alias.fish.nswitch.path;
+    nswitch = builtins.readFile config.sops.secrets.fish-alias.fish.nswitch.path;
   };
   #programs.fish.shellAliases = {
   #    builtins.readFile config.age.secrets.fish-alias.path
