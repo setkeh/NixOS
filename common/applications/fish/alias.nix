64f8fa4c51895435710590a5cc78290dd08451c9
James SETKEH Griffis <setkeh@gmail.com>
2026-04-10 05:36:35 +1000

    More cleanup

--- common/applications/fish/alias.nix
+++
@@ -1,8 +0,0 @@
-({ osConfig, ...}: {
-  # This file sources the decrypted aliases from the sops-nix service.
-  # The secret itself is defined at the system level in flake.nix,
-  # because it requires root permissions to decrypt.
-  programs.fish.extraConfig = {
-    nswitch = builtins.readFile ~/.secrets/nswitch;
-  };
-})
