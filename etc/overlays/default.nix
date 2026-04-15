74772dbcf892ac794cd92f2dd22faa119b4fdad1
James SETKEH Griffis <setkeh@gmail.com>
2026-04-16 03:22:16 +1000

    I forgot the overlay

--- etc/overlays/default.nix
+++ etc/overlays/default.nix
@@ -1,8 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-  nixpkgs = {
+  pkgs = import inputs.nixpkgs {
     overlays = [
+      /* My Custom Package Channel */
+      inputs.nixpkgs-channel.overlays.default
+
+      /* Custom Package Configuration */
       (import ./slstatus.nix)
       (import ./dwm.nix)
       (import ./age.nix)
