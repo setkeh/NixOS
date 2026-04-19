f6e4d0c31d90c5e3c553a8dc09ab4fdc2654af82
James SETKEH Griffis <setkeh@gmail.com>
2026-04-19 22:43:40 +1000

    I Forgot to add the experimental commands

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -22,6 +22,8 @@
   programs.fish.enable = true;
   users.users.setkeh.shell = pkgs.fish;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   /* These packages to make yubikey work */
   environment.systemPackages = with pkgs; [
     wget
