a55d27601b8fc3e71042ec7f52096f6d87c2bafe
James SETKEH Griffis <setkeh@gmail.com>
2026-04-19 19:27:21 +1000

    Adding Fish/tmux Configs to WSL

--- home/wsl/default.nix
+++ home/wsl/default.nix
@@ -1,7 +1,18 @@
 { config, pkgs, ... }: {
   imports = [
+    /* Packages */
     ./packages.nix
+
+    /* Common Configs */
     ../../common/git.nix
+
+    /* Fish Imports */
+    ../../common/applications/fish/init.nix
+    ../../common/applications/fish/plugins.nix
+    ../../common/applications/fish/functions.nix
+
+    /* Tmux Config */
+    ../../common/applications/tmux/e7250.nix
   ];
 
   # Basic user info
