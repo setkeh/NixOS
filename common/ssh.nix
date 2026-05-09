e8a37435a13e870b676ce47f906f3d81524d61c7
James SETKEH Griffis <setkeh@gmail.com>
2026-05-09 17:28:40 +1000

    Stack masters need some legacy config

--- common/ssh.nix
+++ common/ssh.nix
@@ -2,12 +2,17 @@
 
 {
   programs.ssh = {
-    extraConfig = "
-      Host mxl-stack-master
-        HostName 10.0.134.100  # Replace with the actual IP
-        KexAlgorithms +diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
-        # You might also need this if it fails on the host key:
-        HostKeyAlgorithms +ssh-rsa
-    ";
+    extraConfig = "";
+    matchBlocks = {
+     "10.0.134.100" = {
+       hostname = "10.0.134.100";
+       extraOptions = {
+         extraConfig = "
+           KexAlgorithms +diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
+           # You might also need this if it fails on the host key:
+           HostKeyAlgorithms +ssh-rsa";
+       };
+     };
+    };
   };
 }
