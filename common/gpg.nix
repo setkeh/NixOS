9e43e1ac3f9f41074877088e2dcc892e5f5915e6
James SETKEH Griffis <setkeh@gmail.com>
2026-06-17 19:22:34 +1000

    pinentry is a pain in the ass

--- common/gpg.nix
+++ common/gpg.nix
@@ -10,7 +10,7 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-curses; # Or pinentry-gnome3 / pinentry-qt
+    pinentryPackage = pkgs.pinentry-dmenu; #pkgs.pinentry-curses;
     enableExtraSocket = true;
   };
 }
