f65a25e0e6849a28f3c697340ca48ae077bd99df
James SETKEH Griffis <setkeh@gmail.com>
2026-06-17 12:23:03 +1000

    Setting the Ai-Server config up gor gpg forwarding

--- common/ssh.nix
+++ common/ssh.nix
@@ -12,6 +12,16 @@
            # You might also need this if it fails on the host key:
            HostKeyAlgorithms +ssh-rsa";
        };
+
+     "hermes" = {
+        hostname = "10.0.66.75";
+        user = "setkeh";
+        forwardAgent = true;
+        extraOptions = {
+          # Maps your local extra socket to the remote regular socket
+          RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent.extra";
+        };
+      };
      };
     };
   };
