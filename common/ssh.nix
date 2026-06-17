b93950d90ca8759817952abb13b2a2dd3f04ccec
James SETKEH Griffis <setkeh@gmail.com>
2026-06-17 18:56:01 +1000

    Updating ssh config to newer non depricated standard

--- common/ssh.nix
+++ common/ssh.nix
@@ -3,27 +3,37 @@
 {
   programs.ssh = {
     enable = true;
-    extraConfig = "";
-    matchBlocks = {
-     "10.0.134.100" = {
-       hostname = "10.0.134.100";
-       extraOptions = {
-         extraConfig = "
-           KexAlgorithms +diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
-           # You might also need this if it fails on the host key:
-           HostKeyAlgorithms +ssh-rsa";
-       };
+    enableDefaultConfig = false; # Clears the default config deprecation warning
 
-     "10.0.66.75" = {
-        hostname = "10.0.66.75";
-        user = "setkeh";
-        forwardAgent = true;
-        extraOptions = {
-          # Maps your local extra socket to the remote regular socket
-          RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent.extra";
-        };
+    settings = {
+      # Global defaults (applied to all hosts)
+      "*" = {
+        AddKeysToAgent = "no";
+        Compression = "no";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
+        ForwardAgent = "no";
+        HashKnownHosts = "no";
+        ServerAliveCountMax = 3;
+        ServerAliveInterval = 0;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
       };
-     };
+
+      # Your legacy server block
+      "10.0.134.100" = {
+        HostName = "10.0.134.100";
+        KexAlgorithms = "+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1";
+        HostKeyAlgorithms = "+ssh-rsa";
+      };
+
+      # Your AI Server block with the YubiKey tunnel
+      "10.0.66.75" = {
+        HostName = "10.0.66.75";
+        User = "setkeh";
+        ForwardAgent = true;
+        RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent.extra";
+      };
     };
   };
 }
