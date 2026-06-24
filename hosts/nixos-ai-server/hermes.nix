a8d577261431373e5312359ab84ab63ec1ac8be0
James SETKEH Griffis <setkeh@gmail.com>
2026-06-25 08:22:45 +1000

    Adding some more config to hermes

--- hosts/nixos-ai-server/hermes.nix
+++ hosts/nixos-ai-server/hermes.nix
@@ -3,7 +3,19 @@
 {
   services.hermes-agent = {
     enable = true;
-    settings.model.default = "gemini-3.5-flash";
+    settings = {
+      model = {
+        default = "gemini-3.5-flash";
+        provider = "gemini";
+        base_url = "";
+      };
+      providers = {
+        gemini = {
+          provider_type = "google";
+          api_key_env = "GEMINI_API_KEY"; 
+        };
+      };
+    };
     environmentFiles = [ config.sops.secrets."hermes/env".path ];
     addToSystemPackages = true;
   };
