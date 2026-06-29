a6c03f82c856b678a660cb394bba2e1f123c9528
James SETKEH Griffis <setkeh@gmail.com>
2026-06-30 08:56:32 +1000

    Commiting Current iteration

--- hosts/nixos-ai-server/hermes/hermes.nix
+++ hosts/nixos-ai-server/hermes/hermes.nix
@@ -90,34 +90,5 @@
 
     extraDependencyGroups = [ "honcho" "messaging" ];
     settings.memory.provider = "honcho";
-
-    /*profiles = [
-          # Default/orchestrator profile configuration
-          ({
-            config, pkgs, lib, ...
-          }: {
-            hermes-agent.profiles = {
-              coder = {
-                name = "Coder Profile";
-                model.default = "mistralai/devstral-small-2-2512";
-                model.provider = "lmstudio";
-                model.base_url = "http://100.65.62.97:1234/v1";
-
-                toolsets = [ "hermes-cli" "terminal" "file" "code_execution" ];
-                agent.personality = "technical";
-
-                # Add coder-specific settings here
-              };
-
-              researcher = {
-                name = "Researcher Profile";
-                model.default = "gemini-3.1-pro-preview";
-                toolsets = [ "web" "browser" ];
-
-                # Add researcher-specific settings here
-              };
-            };
-          })
-        ];*/
   };
 }
