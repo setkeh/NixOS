85d68b416f8c137c4d7cd66a8dec8e3225bc4a34
James SETKEH Griffis <setkeh@gmail.com>
2026-09-04 04:03:28 +1000

    Merging cups specific etc options

--- hosts/workstation/printing-client.nix
+++ hosts/workstation/printing-client.nix
@@ -2,9 +2,16 @@
 {
   services.printing.enable = false;
 
-  environment.etc."cups/client.conf".text = ''
-    ServerName 10.0.66.75
-  '';
+  environment.etc = {
+    "cups/client.conf".text = ''
+        ServerName 10.0.66.75
+    '';
+
+    /* Setting lpoptions for posteck label printer */
+    "cups/lpoptions".text = ''
+        Dest labels media-type=labels
+    '';
+  }
 
   environment.systemPackages = with pkgs; [
     cups          # lp, lpstat, lpoptions, cancel, plus libcups
