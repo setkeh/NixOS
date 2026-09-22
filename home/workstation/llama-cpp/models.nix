1c62789baf67090fa8905a794ab719e9fb05c921
James SETKEH Griffis <setkeh@gmail.com>
2026-09-22 20:50:21 +1000

    More model testing and tuning

--- home/workstation/llama-cpp/models.nix
+++ home/workstation/llama-cpp/models.nix
@@ -85,6 +85,7 @@ let
         # alive, and voice never sends images. Image turns still work, just slower.
         no-mmproj-offload = true;
         load-mode = "auto";
+        cache-ram = 16384;
         log-verbosity = 4;
       };
     };
